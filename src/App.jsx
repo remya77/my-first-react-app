7de8fed51619968b1cf1b3ff8b0436f210323c7f
Remya Ramesan <Remya.Ramesan@ingka.ikea.com>
2024-09-18 16:25:07 +0200

    todo mapping looping

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import React from 'react';
+
 import './App.css'
 
 const App = () => {
 
-  const todo = {
+  const todos = [{
     text: "do not forget to commit after a change",
+    done: true
+  },
+  {
+    text: "clean the kitchen",
     done: false
-  };
-  const getName =()=>{return "Remya";
-}
+  },
+  {
+    text: "cook food",
+    done: true
+  }];
 
   return (
     <>
-      <h1>Hello, world!</h1>
-      <p>Hello again</p>
-      <p>{todo.text}</p>
-      <p>{getName()}</p>
+      <h1>ToDos</h1>
+      <p>Undone ToDos</p>
+      {todos.map(todo=>{
+        return <p>{todo.done ? "all todos are done" : <p>{todo.text}</p>}</p>
+      })}
+      {/* {todos[0].done ? "all todos are done" : <p>{todos[0].text}</p>}
+      {todos[1].done ? "all todos are done" : <p>{todos[1].text}</p>}
+      {todos[2].done ? "all todos are done" : <p>{todos[2].text}</p>} */}
     </>
   );
 }
